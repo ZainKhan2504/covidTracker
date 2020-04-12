b50cd09e7fa190c761615c16045d4bef14126e8d
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2020-04-12 23:49:28 +0500

    Final Commit

---
+++ build/precache-manifest.be4b23a6e28f4512b5866c9ddb6fbc3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae4d28535cda94854f2af6055c903d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3955a7f00164618b7868",
+    "url": "/static/css/main.fe03294c.chunk.css"
+  },
+  {
+    "revision": "7861cee5d22e894f11a9",
+    "url": "/static/js/2.9a235f53.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.9a235f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3955a7f00164618b7868",
+    "url": "/static/js/main.b0d73212.chunk.js"
+  },
+  {
+    "revision": "8d552397e8e842fab456",
+    "url": "/static/js/runtime-main.35871c95.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
